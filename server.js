244b7f90e33f8eae1bf7625fec19ae4c579839b5
syahrilhanla <syahrilhanla5@gmail.com>
2021-01-04 21:56:51 +0800

    testing heroku deploy

--- server.js
+++ server.js
@@ -296,17 +296,17 @@ if (process.env.NODE_ENV === "production") {
 	// Set static folder
 	app.use(express.static("client/build"));
 
-	// app.get("*", (req, res) => {
-	// 	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-	// });
-
-	app.get("/*", (req, res) => {
-		let url = path.join(__dirname, "../client/build", "index.html");
-		if (!url.startsWith("/app/"))
-			// we're on local windows
-			url = url.substring(1);
-		res.sendFile(url);
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
+
+	// app.get("/*", (req, res) => {
+	// 	let url = path.join(__dirname, "../client/build", "index.html");
+	// 	if (!url.startsWith("/app/"))
+	// 		// we're on local windows
+	// 		url = url.substring(1);
+	// 	res.sendFile(url);
+	// });
 }
 
 const PORT = process.env.PORT || 5000;
@@ -314,6 +314,7 @@ const PORT = process.env.PORT || 5000;
 server.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow
+			.bold
 	)
 );
