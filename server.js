adad208fc8e599264427e4c499a3eb270f40daed
syahrilhanla <syahrilhanla5@gmail.com>
2021-01-04 20:39:55 +0800

    change server static folder setup

--- server.js
+++ server.js
@@ -296,8 +296,16 @@ if (process.env.NODE_ENV === "production") {
 	// Set static folder
 	app.use(express.static("client/build"));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	// app.get("*", (req, res) => {
+	// 	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	// });
+
+	app.get("/*", (req, res) => {
+		let url = path.join(__dirname, "../client/build", "index.html");
+		if (!url.startsWith("/app/"))
+			// we're on local windows
+			url = url.substring(1);
+		res.sendFile(url);
 	});
 }
 
